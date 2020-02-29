5451c50bdb3309596d03b5c00fef1ed79bfa2b70
jellyfishbarbra <jellyfishbarbra@protonmail.com>
2020-02-29 18:13:41 +0100

    ledger: create Transaction/Account structures

--- ledger/ledger.go
+++ ledger/ledger.go
@@ -1,1 +1,15 @@
 package ledger
+
+// Account represents a cryptographic identity holding value on the ledger
+// 		TODO: to specify
+type Account struct {
+}
+
+// Transaction reprensent an attempt at transfering value from one account to another.
+// 		A "Transaction" object comes with no validity guarantees.
+// 		It can represent a canonized tx, a tx waiting for confs, a mempool tx.
+type Transaction struct {
+	origin Account
+	destination Account
+	value float
+}
