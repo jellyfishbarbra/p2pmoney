4b8408238d554fcee8b2d9e19741222c7bb357a3
jellyfishbarbra <jellyfishbarbra@protonmail.com>
2020-03-05 03:22:00 +0100

    client: add fields to configuration + update MOTD

--- client/main.go
+++ client/main.go
@@ -15,19 +15,21 @@ var config = struct {
 	debugMode   bool
 	portPeering int
 	logLevel    log.Level
-}{}
+	connectSeed bool
+	version     string
+}{version: "0.0.1"}
 
 func main() {
 	setup()
-	padlock, err := ioutil.ReadFile("padlock")
+	padlock, err := ioutil.ReadFile("motd")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(string(padlock))
 	log.SetHandler(text.New(os.Stdout))
-	log.Info("initializing client")
+	log.WithField("version", config.version).Info("initializing client")
 	log.Info("initializing peering daemon")
-	peeringDaemon, err := peering.NewDaemon(peering.NewConfig("0.0.1", config.portPeering, config.logLevel))
+	peeringDaemon, err := peering.NewDaemon(peering.NewConfig(config.version, config.portPeering, config.logLevel))
 	if err != nil {
 		log.WithField("error", err.Error()).WithField("port", config.portPeering).Fatal("could not start peering daemon")
 	}
@@ -37,6 +39,11 @@ func main() {
 		log.WithField("error", err.Error()).WithField("port", config.portPeering).Fatal("failed to launch peering daemon")
 	}
 	log.Infof("started peering daemon on port %d", config.portPeering)
+
+	if config.connectSeed {
+		log.Info("[client] requesting connection to seed host")
+		peeringDaemon.Connect("172.105.75.16")
+	}
 	for {
 	}
 }
@@ -44,6 +51,7 @@ func main() {
 func setup() {
 	flag.BoolVar(&config.debugMode, "debug", false, "show debug log")
 	flag.IntVar(&config.portPeering, "port", 5555, "custom peering daemon port")
+	flag.BoolVar(&config.connectSeed, "seed", false, "connect to seed host")
 	flag.Parse()
 
 	if config.debugMode {
